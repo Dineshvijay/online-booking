60974abcb0d3d61c57fed46e3af9e799cdcbf448
Dinesh <Dineshvijay@users.noreply.github.com>
2023-03-24 02:10:31 +0530

    fix: removed hardcoded keys

--- springboot/saloon-api/src/main/java/com/dineshvijay/saloonapi/SaloonApiApplication.java
+++ springboot/saloon-api/src/main/java/com/dineshvijay/saloonapi/SaloonApiApplication.java
@@ -1,8 +1,10 @@
 package com.dineshvijay.saloonapi;
 
 import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,10 +13,11 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 @EnableJpaAuditing
 public class SaloonApiApplication {
-
+	@Autowired
+	private Environment environment;
 	@PostConstruct
 	public void setup() {
-		Stripe.apiKey = "sk_test_51MlPqdSCcfA0lX7eNkGy3odeoDvThEf3AVuSr0JN5pWnBPSCgEhctxK4nZkOD1NxOYcYEiEt6qJhaSY3BWdZdWcW00NdfRkQTa";
+		Stripe.apiKey = environment.getProperty("stripe_test_key");
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SaloonApiApplication.class, args);
